f17c11e68afcec6849ec5984fca044738cabef1b
Ghouse Azam <ghouseazam_s@nexteducation.in>
2026-02-14 12:52:36 +0530

    Initial Commit

---
+++ SignInUI/SignInUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SignInUIApp.swift
+//  SignInUI
+//
+//  Created by next on 14/02/26.
+//
+
+import SwiftUI
+
+@main
+struct SignInUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
